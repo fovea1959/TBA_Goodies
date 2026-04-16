1e29f23add94100a0651bbad8b81d4dcd075470d
Doug Wegscheid <dwegscheid@sbcglobal.net>
2026-04-15 22:22:38 -0400

    Add DisplayElements if needed.

--- XLights.py
+++ XLights.py
@@ -8,11 +8,6 @@ import sys
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element
 
-# TODO!!!!
-TODO = """
-need to make sure a <displayElement> is inserted when adding a Model
-"""
-
 
 class Palette:
     def __init__(self, *, xml_text=None, colors=[]):
@@ -61,12 +56,15 @@ class Palette:
 
 class Effect:
     def __init__(self, *, xml_text=None, **kwargs):
+        super().__setattr__('_initialized', False)
         self.defaults = self.get_defaults()
         self.attribute_names = list(self.defaults.keys())
+        self.set_attr_enabled = True
         if xml_text is not None:
             self._make_props_dict_from_xml_text(xml_text)
         else:
             self._make_props_dict_from_kwargs(**kwargs)
+        self._initialized = True
 
     def _make_props_dict_from_kwargs(self, **kwargs):
         self.props = self.defaults.copy()
@@ -75,25 +73,50 @@ class Effect:
 
     def _make_props_dict_from_xml_text(self, xml_text):
         self.props = {}
-        for nvpair in xml_text.split(","):
-            k, v = nvpair.split('=', 1)
+        for nv_pair in xml_text.split(","):
+            k, v = nv_pair.split('=', 1)
             v = v.replace('&comma;', ',')
             self.props[k] = v
 
-    def set_prop(self, *, force=False, key=None, value=None):
-        logging.debug("looking to see what matches %s", key)
-        ok = False
-        if force or key in self.attribute_names:
-            self.props[key] = value
+    def __setattr__(self, key, value):
+        if not getattr(self, '_initialized', False):
+            super().__setattr__(key, value)
+            return
+        key1 = self._lookup_key(key)
+        print(f"{key} -> {key1}")
+        if key1 is None:
+            super().__setattr__(key, value)
         else:
-            for k1 in self.attribute_names:
-                if k1.endswith(f"_{key}"):
-                    self.props[k1] = value
-                    ok = True
-                    logging.debug("%s matches %s", k1, key)
-                    break
-            if not ok:
-                logging.error("what is option %s", key)
+            self.set_prop(key=key1, value=value)
+
+    def __getattr__(self, key):
+        if not getattr(self, '_initialized', False):
+            return super().__getattr__(key)
+        key = self._lookup_key(key)
+        return self.props.get(key)
+
+    def _lookup_key(self, key):
+        if key in self.attribute_names:
+            return key
+        if key in self.props.keys():
+            return key
+        u_key = key.upper()
+        for k1 in self.attribute_names:
+            if k1.upper().endswith(f"_{u_key}"):
+                return k1
+        for k1 in self.props.keys():
+            if k1.upper().endswith(f"_{u_key}"):
+                return k1
+        logging.error("what is option %s", key)
+        return None
+
+    def set_prop(self, *, force=False, key=None, value=None):
+        if force:
+            self.props[key] = value
+            return
+
+        key = self._lookup_key(key)
+        self.props[key] = value
 
     def get_defaults(self):
         return {}
@@ -130,53 +153,57 @@ class TextEffect(Effect):
         'T_TEXTCTRL_Fadein': 1.00,
         'T_TEXTCTRL_Fadeout': 1.00
     }
+
     def get_defaults(self):
-        return self.defaults;
+        return self.defaults
 
 
 class MarqueeEffect(Effect):
-    def get_defaults(self):
-        return {
-            'E_CHECKBOX_Marquee_PixelOffsets': 0,
-            'E_CHECKBOX_Marquee_Reverse': 0,
-            'E_CHECKBOX_Marquee_WrapX': 0,
-            'E_CHECKBOX_Marquee_WrapY': 0,
-            'E_NOTEBOOK_Marquee': 'Settings',
-            'E_SLIDER_MarqueeXC': 0,
-            'E_SLIDER_MarqueeYC': 0,
-            'E_SLIDER_Marquee_Band_Size': 4,
-            'E_SLIDER_Marquee_ScaleX': 100,
-            'E_SLIDER_Marquee_ScaleY': 100,
-            'E_SLIDER_Marquee_Skip_Size': 2,
-            'E_SLIDER_Marquee_Speed': 3,
-            'E_SLIDER_Marquee_Stagger': 0,
-            'E_SLIDER_Marquee_Start': 0,
-            'E_SLIDER_Marquee_Thickness': 1,
-            'T_TEXTCTRL_Fadein': 1.00,
-            'T_TEXTCTRL_Fadeout': 1.00
-        }
+    defaults = {
+        'E_CHECKBOX_Marquee_PixelOffsets': 0,
+        'E_CHECKBOX_Marquee_Reverse': 0,
+        'E_CHECKBOX_Marquee_WrapX': 0,
+        'E_CHECKBOX_Marquee_WrapY': 0,
+        'E_NOTEBOOK_Marquee': 'Settings',
+        'E_SLIDER_MarqueeXC': 0,
+        'E_SLIDER_MarqueeYC': 0,
+        'E_SLIDER_Marquee_Band_Size': 4,
+        'E_SLIDER_Marquee_ScaleX': 100,
+        'E_SLIDER_Marquee_ScaleY': 100,
+        'E_SLIDER_Marquee_Skip_Size': 2,
+        'E_SLIDER_Marquee_Speed': 3,
+        'E_SLIDER_Marquee_Stagger': 0,
+        'E_SLIDER_Marquee_Start': 0,
+        'E_SLIDER_Marquee_Thickness': 1,
+        'T_TEXTCTRL_Fadein': 1.00,
+        'T_TEXTCTRL_Fadeout': 1.00
+    }
 
+    def get_defaults(self):
+        return self.defaults
 
 class PicturesEffect(Effect):
+    defaults = {
+        'E_CHECKBOX_Pictures_PixelOffsets': 0,
+        'E_CHECKBOX_Pictures_Shimmer': 0,
+        'E_CHECKBOX_Pictures_TransparentBlack': 0,
+        'E_CHECKBOX_Pictures_WrapX': 0,
+        'E_CHOICE_Pictures_Direction': 'none',
+        'E_CHOICE_Scaling': 'No Scaling',
+        'E_FILEPICKER_Pictures_Filename': None,
+        'E_SLIDER_PicturesXC': 0,
+        'E_SLIDER_PicturesYC': 0,
+        'E_SLIDER_Pictures_EndScale': 100,
+        'E_SLIDER_Pictures_StartScale': 100,
+        'E_TEXTCTRL_Pictures_FrameRateAdj': 1.0,
+        'E_TEXTCTRL_Pictures_Speed': 1.0,
+        'E_TEXTCTRL_Pictures_TransparentBlack': 0,
+        'T_TEXTCTRL_Fadein': 1.00,
+        'T_TEXTCTRL_Fadeout': 1.00
+    }
+
     def get_defaults(self):
-        return {
-            'E_CHECKBOX_Pictures_PixelOffsets': 0,
-            'E_CHECKBOX_Pictures_Shimmer': 0,
-            'E_CHECKBOX_Pictures_TransparentBlack': 0,
-            'E_CHECKBOX_Pictures_WrapX': 0,
-            'E_CHOICE_Pictures_Direction': 'none',
-            'E_CHOICE_Scaling': 'No Scaling',
-            'E_FILEPICKER_Pictures_Filename': None,
-            'E_SLIDER_PicturesXC': 0,
-            'E_SLIDER_PicturesYC': 0,
-            'E_SLIDER_Pictures_EndScale': 100,
-            'E_SLIDER_Pictures_StartScale': 100,
-            'E_TEXTCTRL_Pictures_FrameRateAdj': 1.0,
-            'E_TEXTCTRL_Pictures_Speed': 1.0,
-            'E_TEXTCTRL_Pictures_TransparentBlack': 0,
-            'T_TEXTCTRL_Fadein': 1.00,
-            'T_TEXTCTRL_Fadeout': 1.00
-        }
+        return self.defaults
 
 
 class EffectReference:
@@ -277,11 +304,28 @@ class Model:
         ET.dump(rv)
         return rv
 
+
+class DisplayElement:
+    def __init__(self, *, xml_element = None, name = None):
+        if xml_element is not None:
+            self.xml_element = xml_element
+        else:
+            self.xml_element = ET.Element('Element', {
+                'name': name,
+                'type': 'model',
+                'collapsed': '0',
+                'visible': '1',
+            })
+
+    def xml(self):
+        return self.xml_element
+
 class Sequence:
     def __init__(self):
         self.palettes = []
         self.effects = []
         self.models = {}
+        self.display_elements = {}
 
         self.xml_doc = None
 
@@ -302,6 +346,12 @@ class Sequence:
         e = self.xml_doc.find("./EffectDB")
         for e1 in list(e):
             self.effects.append(e1.text)
+            e.remove(e1)
+
+        self.display_elements = {}
+        e = self.xml_doc.find("./DisplayElements")
+        for e1 in list(e):
+            self.display_elements[e1.attrib.get('name')] = DisplayElement(xml_element=e1)
             e.remove(e1)
 
         self.models = {}
@@ -342,6 +392,11 @@ class Sequence:
             EffectReference(sequence=self, effect=effect, name=name, start=start, end=end, palette=palette)
         )
 
+        display_element = self.display_elements.get(model_name)
+        if display_element is None:
+            display_element = DisplayElement(name=model_name)
+            self.display_elements[model_name] = display_element
+
 
     def xml_tree(self) -> ET.ElementTree:
         rv = copy.deepcopy(self.xml_doc)
@@ -360,6 +415,11 @@ class Sequence:
         for f in self.effects:
             e.append(f.xml())
 
+        # add the display elements into the document
+        e = rv.find("./DisplayElements")
+        for f in self.display_elements.values():
+            e.append(f.xml())
+
         # add the models back into the document
         e = rv.find("./ElementEffects")
         for f in self.models.values():
@@ -376,11 +436,18 @@ def main(argv):
 
     effect = TextEffect(Text='3620')
     palette = Palette(colors=('#FF0000', '#00FF00'))
+    print(f'effect.text = {effect.text}')
+    print(f'effect.xxx = {effect.xxx}')
+    effect.text = 'xxxxx'
+    print(f'effect.text = {effect.text}')
     sequence.add_effect(model_name="Matrix", layer_index=1, effect=effect, palette=palette, start=1000, end=9000)
+
+    sequence.add_effect(model_name="surprise", layer_index=1, effect=effect, palette=palette, start=1000, end=9000)
 
     sequence.length = 10.0
     xml_tree = sequence.xml_tree()
-    xml_tree.write('xlights_test.xsq')
+    ET.dump(xml_tree)
+    # xml_tree.write('xlights_test.xsq')
 
 
 if __name__ == '__main__':
